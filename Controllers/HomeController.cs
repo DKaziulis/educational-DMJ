af356680d215e93104685b02b277012ced7fde6e
just7nz <zidonis.justinas@gmail.com>
2023-09-25 20:36:22 +0300

    Added a file upload tab, able to choose files from local device (not functional yet)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,5 +34,45 @@ namespace Student_Planner.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> FileUpload(IFormFile file)
+        {
+            await UploadFile(file);
+            TempData["msg"] = "File Uploaded successfully.";
+            return View();
+        }
+        // Upload file on server
+        public async Task<bool> UploadFile(IFormFile file)
+        {
+            string path = "";
+            bool iscopied = false;
+            try
+            {
+                if (file.Length > 0)
+                {
+                    string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Upload"));
+                    using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                    {
+                        await file.CopyToAsync(filestream);
+                    }
+                    iscopied = true;
+                }
+                else
+                {
+                    iscopied = false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return iscopied;
+        }
     }
 }
---
+++ Views/Home/FileUpload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Student_Planner.Views.Home
+{
+    public class FileUploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
