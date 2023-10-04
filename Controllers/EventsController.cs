fe988b4452ab15817e868c3b4e049f580a51a27f
Deividas_K <deividaskaziulis1@gmail.com>
2023-10-04 11:59:06 +0300

    Added the ability to save events into a JSON file, and removed some nullable reference warnings in the Event Model, as well as added a couple of properties to the Event class

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,20 +1,53 @@
 ﻿using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Student_Planner.Models;
-
-
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace Student_Planner.Controllers
 {
     public class EventsController : Controller
     {
         private static List<Event> events = new List<Event>();
+        private string jsonData = "";
+        private static string eventDataFilePath = Path.GetFullPath(Path.Combine
+            (Path.Combine(Directory.GetCurrentDirectory(), "EventData"), "Test.json"));
 
+        //Method for serializing data into a JSON file in the EventData folder
+        private void SerializeToJson(string jsonData)
+        {
+            // Serialize the list of events to JSON
+            jsonData = JsonSerializer.Serialize(events);
+            // Write the JSON data to the file
+            System.IO.File.WriteAllText(eventDataFilePath, jsonData);
+        }
+
+        //Deserialize the JSON file data back into an events list if the file exists
+        private List<Event> DeserializeFromJSON(string json, List<Event> events)
+        {
+            if (System.IO.File.Exists(eventDataFilePath))
+            {
+                jsonData = System.IO.File.ReadAllText(eventDataFilePath);
+
+                if (!string.IsNullOrEmpty(jsonData))
+                {
+                    events = JsonSerializer.Deserialize<List<Event>>(jsonData);
+                }
+                else
+                {
+                    // Handle the case where jsonData is empty
+                    events.Clear();
+                }
+            }
+            return events;
+        }
 
         public ActionResult Index()
         {
+            events = DeserializeFromJSON(jsonData, events);
+
             // Sort events by date before displaying.
-            var sortedEvents = events.OrderBy(e => e.Date).ToList();
+            var sortedEvents = events.OrderBy(e => e.StartTime).ToList();
             return View(sortedEvents);
         }
 
@@ -32,6 +65,9 @@ namespace Student_Planner.Controllers
             {
                 newEvent.Id = events.Count + 1; // unique ID
                 events.Add(newEvent);
+
+                SerializeToJson(jsonData);
+
                 return RedirectToAction("Index");
             }
             return View(newEvent);
@@ -57,8 +93,10 @@ namespace Student_Planner.Controllers
                 {
                     // Update event properties.
                     existingEvent.Name = updatedEvent.Name;
-                    existingEvent.Date = updatedEvent.Date;
+                    existingEvent.StartTime = updatedEvent.StartTime;
                     existingEvent.Description = updatedEvent.Description;
+
+                    SerializeToJson(jsonData);
                 }
                 return RedirectToAction("Index");
             }
@@ -85,6 +123,7 @@ namespace Student_Planner.Controllers
             if (existingEvent != null)
             {
                 events.Remove(existingEvent);
+                SerializeToJson(jsonData);
                 return RedirectToAction("Index");
             }
             return NotFound();
--- Models/Event.cs
+++ Models/Event.cs
@@ -4,15 +4,12 @@ namespace Student_Planner.Models
 {
     public class Event
     {
-        public uint EventID { get; set; }
-        public string EventName { get; set; } = string.Empty;
-        public string EventDescription { get; set; } = string.Empty;
-        public DateTime EventStartTime { get; set; }
-        public DateTime EventEndTime { get; set; }
-        public TimeSpan EventDuration { get; set; }
         public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime Date { get; set; }
-        public string Description { get; set; }
+        [MaxLength(60)]
+        public string? Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EventEndTime { get; set; }
+        public string? Description { get; set; }
+        public TimeSpan EventDuration { get; set; }
     }
 }
