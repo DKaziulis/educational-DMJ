3ed24aeab1e927a48c69f25b89f4e68d2ba4700c
just7nz <zidonis.justinas@gmail.com>
2023-10-23 00:45:54 +0300

    Small custom extension method for sorting events

---
+++ Services/SortEvents.cs
@@ -0,0 +1,15 @@
+﻿using Student_Planner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Student_Planner.Services
+{
+    public static class Extension
+    {
+        public static List<Event> SortEvents(this List<Event> events)
+        {
+                return events.OrderBy(e => e.BeginDate).ToList();
+        }
+    }
+}
