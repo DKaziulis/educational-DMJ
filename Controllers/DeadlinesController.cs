f1c5ee307053c49e64b2549f2d9f7aa9b01cb6d0
MARTO <118682717+MartoLedas@users.noreply.github.com>
2023-10-24 00:20:58 +0300

    new home background

--- Controllers/DeadlinesController.cs
+++ Controllers/DeadlinesController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student_Planner.Models;
 
-namespace Student_Planner.Controllers
+namespace Student_Planner.Controllers;
+
+public class DeadlinesController : Controller
 {
-    public class DeadlinesController : Controller
+    public ActionResult Index()
     {
-        public IActionResult Index()
+        var deadlines = new List<Deadline>
         {
-            return View();
-        }
+            new("Task 1", new DateTime(2023, 10, 25, 23, 59, 59)),
+            new("Task 2", new DateTime(2023, 10, 24, 18, 0, 0)),
+            new("Task 3", new DateTime(2023, 10, 26, 23, 59, 59))
+        };
+
+        deadlines.Sort(); // Sort the deadlines based on due date
+
+        return View(deadlines);
     }
+
+    // Other controller actions (Create, Edit, Delete) can be added here
 }
--- Models/Deadline.cs
+++ Models/Deadline.cs
@@ -1,6 +1,22 @@
-﻿namespace Student_Planner.Models
+﻿
+
+namespace Student_Planner.Models
 {
-    public class Deadline
+    public class Deadline : IComparable<Deadline>
     {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Deadline(string title, DateTime dueDate)
+        {
+            Title = title;
+            DueDate = dueDate;
+        }
+
+        public int CompareTo(Deadline other)
+        {
+            if (other == null) return 1;
+            return DueDate.CompareTo(other.DueDate);
+        }
     }
 }
