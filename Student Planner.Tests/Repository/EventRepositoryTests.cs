0602d830200af09db615ca62ccffcd2e2132a7d3
MARTO <118682717+MartoLedas@users.noreply.github.com>
2023-12-13 13:26:14 +0200

    43.8%

--- Student Planner.Tests/Repository/EventRepositoryTests.cs
+++ Student Planner.Tests/Repository/EventRepositoryTests.cs
@@ -36,46 +36,9 @@ public class EventRepositoryTests
             // Assert
             Assert.Equal(events.Count, result.Count());
         }
-
-        //[Fact]
-        //void GetById_ReturnsEventById()
-        //{
-        //    // Arrange
-        //    var events = new List<Event>
-        //    {
-        //        new Event { Id = 1, Name = "Event 1" },
-        //        new Event { Id = 2, Name = "Event 2" },
-        //        new Event { Id = 3, Name = "Event 3" }
-        //    };
-
-        //    var mockDbContext = new Mock<EventsDBContext>();
-        //    mockDbContext.Setup(x => x.Events).ReturnsDbSet(events); // Use ReturnsDbSet extension method for DbSet
-
-        //    var eventRepository = new EventRepository(mockDbContext.Object);
-
-        //    // Act
-        //    var result = eventRepository.GetById(2);
-
-        //    // Assert
-        //    Assert.NotNull(result);
-        //    Assert.Equal(2, result.Id);
-        //}
-
-        //[Fact]
-        //void Add_AddsEventToDbContext()
-        //{
-        //    // Arrange
-        //    var eventToAdd = new Event { Id = 1, Name = "New Event" };
-
-        //    var dbContextMock = new Mock<EventsDBContext>();
-        //    var eventRepository = new EventRepository(dbContextMock.Object);
-
-        //    // Act
-        //    eventRepository.Add(eventToAdd);
-
-        //    // Assert
-        //    dbContextMock.Verify(x => x.Events.Add(eventToAdd), Times.Once);
-        //}
     }
+
+    
+
 
 }
