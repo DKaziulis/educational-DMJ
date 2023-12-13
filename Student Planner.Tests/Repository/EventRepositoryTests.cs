dc01ed3c69f99ce816da789e1530a51df7ddd402
MARTO <118682717+MartoLedas@users.noreply.github.com>
2023-12-13 13:32:28 +0200

    46.6%

--- Student Planner.Tests/Repository/EventRepositoryTests.cs
+++ Student Planner.Tests/Repository/EventRepositoryTests.cs
@@ -1,44 +1,139 @@
-﻿using System;
+﻿using Xunit;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using Student_Planner.Databases;
 using Student_Planner.Models;
 using Student_Planner.Repositories.Implementations;
-using Xunit;
+using System.Linq;
 
 public class EventRepositoryTests
 {
-    [Fact]
-    public void GetAll_ReturnsAllEvents()
+    private readonly EventRepository _eventRepository;
+    private readonly EventsDBContext _dbContext;
+
+    public EventRepositoryTests()
     {
-        // Arrange
         var options = new DbContextOptionsBuilder<EventsDBContext>()
-            .UseInMemoryDatabase(databaseName: "InMemoryEventDB")
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Use a unique name for each database
             .Options;
 
-        using (var dbContext = new EventsDBContext(options))
-        {
-            var events = new List<Event>
-            {
-                new Event { Id = 1, Name = "Event 1" },
-                new Event { Id = 2, Name = "Event 2" },
-                new Event { Id = 3, Name = "Event 3" }
-            };
-
-            dbContext.Events.AddRange(events);
-            dbContext.SaveChanges();
-
-            var eventRepository = new EventRepository(dbContext);
-
-            // Act
-            var result = eventRepository.GetAll();
-
-            // Assert
-            Assert.Equal(events.Count, result.Count());
-        }
+        _dbContext = new EventsDBContext(options);
+        _eventRepository = new EventRepository(_dbContext);
     }
 
-    
+    [Fact]
+    public void GetAll_ShouldReturnAllEvents()
+    {
+        // Arrange
+        var event1 = new Event { Id = 1, DayId = 1 };
+        var event2 = new Event { Id = 2, DayId = 2 };
+        _dbContext.Events.AddRange(event1, event2);
+        _dbContext.SaveChanges();
 
+        // Act
+        var result = _eventRepository.GetAll();
 
+        // Assert
+        Assert.Equal(2, result.Count());
+    }
+
+    [Fact]
+    public void GetById_ShouldReturnEventIfExists()
+    {
+        // Arrange
+        var _event = new Event { Id = 1, DayId = 1 };
+        _dbContext.Events.Add(_event);
+        _dbContext.SaveChanges();
+
+        // Act
+        var result = _eventRepository.GetById(_event.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(_event.Id, result.Id);
+    }
+
+    [Fact]
+    public void Add_ShouldAddNewEvent()
+    {
+        // Arrange
+        var _event = new Event { Id = 1, DayId = 1 };
+
+        // Act
+        _eventRepository.Add(_event);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var result = _dbContext.Events.Find(_event.Id);
+        Assert.NotNull(result);
+        Assert.Equal(_event.Id, result.Id);
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateExistingEvent()
+    {
+        // Arrange
+        var _event = new Event { Id = 1, DayId = 1 };
+        _dbContext.Events.Add(_event);
+        _dbContext.SaveChanges();
+        _event.DayId = 2;
+
+        // Act
+        _eventRepository.Update(_event);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var result = _dbContext.Events.Find(_event.Id);
+        Assert.NotNull(result);
+        Assert.Equal(_event.DayId, result.DayId);
+    }
+
+    [Fact]
+    public void Delete_ShouldRemoveExistingEvent()
+    {
+        // Arrange
+        var _event = new Event { Id = 1, DayId = 1 };
+        _dbContext.Events.Add(_event);
+        _dbContext.SaveChanges();
+
+        // Act
+        _eventRepository.Delete(_event);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var result = _dbContext.Events.Find(_event.Id);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetByDayId_ShouldReturnEventIfExists()
+    {
+        // Arrange
+        var _event = new Event { Id = 1, DayId = 1 };
+        _dbContext.Events.Add(_event);
+        _dbContext.SaveChanges();
+
+        // Act
+        var result = _eventRepository.GetByDayId(_event.DayId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(_event.DayId, result.DayId);
+    }
+
+    [Fact]
+    public void GetAllByDayId_ShouldReturnAllEventsForDay()
+    {
+        // Arrange
+        var event1 = new Event { Id = 1, DayId = 1 };
+        var event2 = new Event { Id = 2, DayId = 1 };
+        var event3 = new Event { Id = 3, DayId = 2 };
+        _dbContext.Events.AddRange(event1, event2, event3);
+        _dbContext.SaveChanges();
+
+        // Act
+        var result = _eventRepository.GetAllByDayId(1);
+
+        // Assert
+        Assert.Equal(2, result.Count());
+    }
 }
